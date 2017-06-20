07e5c9ae722f258a3cf181844adba9739391e16b
Hector Duran <hectormdurans@gmail.com>
2017-06-20 18:46:38 -0400

    autenticacion

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layaout')
+
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Welcome</div>
+				<div class="panel-body">
+				   Bienvenido al Sistema
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
